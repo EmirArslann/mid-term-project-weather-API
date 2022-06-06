6d4a334aa565d39c1bb3b9d6428cad01f8783fe8
atesmen <63913174+atesmen@users.noreply.github.com>
2022-06-06 16:47:29 -0700

    xx

---
+++ javascripts/fetch.js
@@ -0,0 +1,5 @@
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={86af47ab6aa81e36b78f0c3ee3ca0315}
+
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={86af47ab6aa81e36b78f0c3ee3ca0315}
+require('dotenv').config();
+console.log(process.env) 
