fca4fdce64d94b16756cd95cc429f2771e2a2bd7
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-06-07 14:15:47 -0700

    update

--- javascripts/fetch.js
+++ javascripts/fetch.js
@@ -1,4 +1,3 @@
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=9af9ddf70ec9576dc6e6de7f4574bbb1
+import config from "./api.js";
 
-
-https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=9af9ddf70ec9576dc6e6de7f4574bbb1
+console.log(config.WEATHER_API_KEY);
