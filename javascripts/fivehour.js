f132068aec3305c4b13d28c7a2dfcb3f67649b36
atesmen <63913174+atesmen@users.noreply.github.com>
2022-06-08 17:00:43 -0700

    3hour range

---
+++ javascripts/fivehour.js
@@ -0,0 +1,34 @@
+import config from "./api.js";
+
+console.log(config.WEATHER_API_KEY);
+
+
+
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=49.246292&lon=-123.116226&units=metric&appid=${config.WEATHER_API_KEY}`)
+.then(response => response.json())
+.then(data => {
+    let dates = data.list.filter(datetxt => {
+        return   datetxt.dt_txt.startsWith('2022-06-09')
+    })
+  let thour = document.querySelector('.hour')
+    dates.forEach(element => {
+        var iconurl = "http://openweathermap.org/img/w/" + element.weather[0].icon + ".png";
+        thour.innerHTML += `
+        <div class="hour-box">
+        <h3>${element.dt_txt.split(' ').pop()}</h3>
+        <p>${element.main.feels_like}°C</p>
+        <p>H:${element.main.temp_max}</p>
+        <p>L:${element.main.temp_min}</p>
+        <p>${element.weather[0].description}</p>
+        <img src="${iconurl}" alt="">
+        </div>
+        `
+    });
+    dates.forEach(element => {
+        
+    });
+
+    console.log(dates)
+    console.log(data)
+});
+
